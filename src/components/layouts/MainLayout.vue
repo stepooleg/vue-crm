aa7e8022ce06cb172a03322dcd4c332afdd1f39f
StepanenkoOV <o.stepanenko@id-mt.ru>
2019-07-03 15:03:54 +0300

    add datetime filter and on dropdown button

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="app-main-layout">
-    <navbar @click="isOpen = !isOpen"/>
-    <sidebar :isOpen="isOpen"/>
-    <main class="app-content" :class="{full: !isOpen}">
-      <div class="app-page">
-        <router-view/>
-      </div>
-    </main>
-  </div>
+    <div class="app-main-layout">
+        <navbar @click="isOpen = !isOpen"/>
+        <sidebar :isOpen="isOpen"/>
+        <main class="app-content" :class="{full: !isOpen}">
+            <div class="app-page">
+                <router-view/>
+            </div>
+        </main>
+
+        <div class="fixed-action-btn">
+            <router-link class="btn-floating btn-large blue" to="/record">
+                <i class="large material-icons">add</i>
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dateFilter from './filters/dateFilter'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
 
+Vue.filter('dateFilter', dateFilter)
 new Vue({
   router,
   store,
